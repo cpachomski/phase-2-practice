4d47a3f56e6e666a56df9721b43308aa8a6514b5
cpachomski <cpachomski@gmail.com>
2015-02-16 21:27:48 -0500

    create migrations and models with associations and secure password

---
+++ db/migrate/20150216212154_create_dreams.rb
@@ -0,0 +1,11 @@
+class CreateDreams < ActiveRecord::Migration
+  def change
+    create_table :dreams do |t|
+      t.text :title
+      t.text :content
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
