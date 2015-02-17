ac5ca1097b637344dfc87b4b7ad0bec3fff0934a
cpachomski <cpachomski@gmail.com>
2015-02-16 21:20:55 -0500

    finish login form on index view

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -1,3 +1,23 @@
-get '/login' do
+get '/index' do
+
+  erb :index
 
 end
+
+
+post '/login' do
+
+  #create a user session to bring along...:{o
+  session[:user] = User.find_by(email: params[:email], password: params[:password] )
+
+  redirect '/users/id'
+
+end
+
+
+post '/signup' do
+
+
+end
+
+
